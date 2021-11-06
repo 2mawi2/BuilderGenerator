97ba13e79e09903eb3c52dd12b237efb69b6ac8e
Marius Wichtner <marius.wichtner@maibornwolff.de>
2021-11-06 11:58:48 +0100

    Initial Commit

---
+++ BuilderGenerator/BuilderGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuilderGeneratorApp.swift
+//  BuilderGenerator
+//
+//  Created by Marius Wichtner on 06.11.21.
+//
+
+import SwiftUI
+
+@main
+struct BuilderGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuilderGenerator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BuilderGenerator
+//
+//  Created by Marius Wichtner on 06.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
